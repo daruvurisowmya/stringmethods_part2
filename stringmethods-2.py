18072a3b90b1245de5ef6678fd43f26e7f463c73
daruvurisowmya <109509763+daruvurisowmya@users.noreply.github.com>
2022-09-30 14:36:37 +0530

    Add files via upload

---
+++ stringmethods-2.py
@@ -0,0 +1,52 @@
+'''Join'''
+#a="welcome to python"
+#print(a.split(' '))
+#print(a.split(" "))
+#print(a.split())
+#print(a.split(""))  #valueError:empty seperator
+
+#a,b,c=3,4
+#a,b,c=3,4,5,6
+
+#a=[int(z) for z in input().split()]
+#a=[int(z) for z in input().split(',')]
+#a=[int(z) for z in input().split(':')]
+#print("the sum is"+' '+str(sum(a)))
+#print("the sum is"+''+str(sum(a)))
+#print("the sum is",sum(a))
+
+#d="hello welcome to joshinnovations"
+#print(d.split())
+#b="hello:welcome: to:josh:innovations"
+#print(b.split(":"))
+#v="python#course#programming#skills"
+#print(v.split('#'))
+
+#a='hi\sowmya'
+#print(a)
+#a='hi\ sowmya'
+#print(a)
+#a='hi\\sowmya'
+#print(a)
+#a=r'hi\sowmya'
+#print(a)
+#a=r"hello\python"
+#print(a,type(a))
+
+#k="hello!\nwelcome to \njoshinnovations"
+#print(k.splitlines())
+#y="hello!\nwelcome to \njoshinnovations"
+#print(y.splitlines(True))
+
+#j="hello what are you doing?"
+#print(j.partition("are"))
+#j="hello what are you doing?"
+#r=j.partition("you")
+#print(r)
+#print(r[0])
+
+#m="We are Learning Python programming , Python is basic need  \ for ML,DS,AI,DL,BigData....etc"
+#print(m.partition("python"))
+#print(m.rpartition("python"))
+
+#print(dir(str))
